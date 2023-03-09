9eb5e17cd7b2d0ea0924b09f994851c1d88c6aaa
'김민형' <'alsgud1515@naver.com'>
2023-03-09 18:27:52 +0900

    2023-03-09

--- src/main/java/com/my/spring/web/ch03/ex02/UserController.java
+++ src/main/java/com/my/spring/web/ch03/ex02/UserController.java
@@ -10,30 +10,32 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @Controller("ch03.ex02")
 @RequestMapping("ch03/ex02/user")
 public class UserController {
-	@Value("${attachPath}") private String attachPath;
+	@Value("${attachPath") private String attachPath;
 	
 	@GetMapping
 	public String userIn() {
 		return "ch03/ex02/userIn";
 	}
-	// 얼굴사진은 binary data이기에 Post를 이용한다.
+	
 	@PostMapping
-	public String userOut(UserDto userDto, HttpServletRequest request, User user) {
+	public String userOut(UserDto userDto, User user) {
+		// 첨부 파일의 이름을 가져옵니다.
 		String filename = userDto.getFace().getOriginalFilename();
+		// 첨부 파일을 저장합니다.
 		saveFile(attachPath + "/" + filename, userDto.getFace());
-		
+		// 유저 객체에 얼굴 이미지 파일 이름을 저장합니다.
 		user.setFaceFilename(filename);
-		
+		// "ch03/ex02/userOut" 뷰를 반환합니다.
 		return "ch03/ex02/userOut";
 	}
 	
+	// 파일을 저장하는 메소드
 	private void saveFile(String filename, MultipartFile file) {
 		try {
+			// 첨부 파일을 지정된 파일 이름으로 저장합니다.
 			file.transferTo(new File(filename));
 		} catch(IOException e) {}
 	}
